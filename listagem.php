73030e68e9bf4d5beaa9ea1143f2d55015f21390
Paula <pmagalhaesa@hotmail.com>
2017-08-27 20:15:11 -0300

    criacao da lista de perfis cpom foreach

--- listagem.php
+++ listagem.php
@@ -7,9 +7,24 @@
   <body>
     <?php
     include 'conexao.php';
-    $result = $mysqli->query("SELECT DISTINCT descricao FROM perfil");
-    $perfis= mysqli_fetch_all($result, MYSQLI_ASSOC);
-    var_dump($perfis);
+    $result = $mysqli->query("SELECT DISTINCT id, descricao FROM perfil");
+    $perfis = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    // echo "<pre>"; print_r($perfis); echo "</pre>";
      ?>
+    <table>
+      <thead>
+        <th>id</th>
+        <th>descricao</th>
+      </thead>
+      <tbody>
+        <?php foreach($perfis as $perfil){   ?>
+          <tr>
+            <td><?php echo $perfil["id"] ?></td>
+            <td><?php echo $perfil["descricao"] ?></td>
+          </tr>
+      <?php  }
+         ?>
+      </tbody>
+    </table>
   </body>
 </html>
